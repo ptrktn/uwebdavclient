b396cfd61c184f36c1c829e3adef22a4ce60186d
ptrktn <ptrktn@users.noreply.github.com>
2022-02-07 19:45:27 +0900

    Use parameters for package and user name

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 import setuptools
 
-gituser = "ptrktn"
+githubuser = "ptrktn"
+name = "uwebdavclient"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 if __name__ == "__main__":
-    setuptools.setup(py_modules=["uwebdavclient"],
-          name="uwebdavclient",
+    setuptools.setup(py_modules=[name],
+          name=name,
           version="0.0.1",
-          author=f"{gituser}@github",
-          author_email="{gituser}@users.noreply.github.com",
-          url=f"https://github.com/{gituser}/{name}",
+          author=f"{githubuser}@github",
+          author_email="{githubuser}@users.noreply.github.com",
+          url=f"https://github.com/{githubuser}/{name}",
           description="Minimal WebDAV client in pure Python",
           long_description=long_description,
           project_urls={
-              "Bug Tracker": f"https://github.com/{gituser}/{name}/issues",
+              "Bug Tracker": f"https://github.com/{githubuser}/{name}/issues",
           },
           classifiers=[
               "Programming Language :: Python :: 3",
